995b974f53573d498d54e0807a53cdc08f4d2782
Sayyed Ehsan Taheri Javid <ehsantaheri25@yahoo.com>
2019-09-26 10:34:07 +0330

    Fix possible "list index out of range" error

--- pythonds/list_index_operator_time_complexity_test.py
+++ pythonds/list_index_operator_time_complexity_test.py
@@ -12,6 +12,6 @@ listIndex = Timer("test(x, r)", "from __main__ import test, x, r")
 
 for i in range(1000000, 100000001, 1000000):
     x = list(range(i))
-    r = random.randint(1, i)
+    r = random.randint(1, i - 1)
     tt = listIndex.timeit(number=1000)
     print("for list of size %d it took %15.5f to get x[%d]" %(i, tt, r))
